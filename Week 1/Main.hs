a9fe3c6f63cfc745c026330ebd58e32d82a34ca5
Tomaszal <mrtomaszal@gmail.com>
2019-01-24 12:28:10 +0000

    Rewrote "Week 1" for easier readability.

--- Week 1/Main.hs
+++ Week 1/Main.hs
@@ -2,53 +2,27 @@ import Functions
 
 main = do
 
-    -- Exercise 1
+    let setBool = [True,True,True,False,False]
+        setInt = [1,2,3,4,3,2,1]
+        setChar = "A man, a plan, a canal: Panama"
 
     mapM_ putStrLn [
-            "Exercise 1 answers:",
-            unlines $ zipWith (\n x -> show n ++ ") " ++ show x) [1..] [
-                8>9,
-                9>=0,
-                True && (True || False),
-                not (not True) == True,
-                countElems 'b' "It was the best of times, it was the blurst of times" == 6,
-                countElems '2' "5438275439872242142" < 3,
-                countElems 't' "The Importance of Being Earnest" > countElems 't' "The Picture of Dorian Gray",
-                countElems 't' "Gadsby" < countElems 's' "The quick brown fox jumped over the lazy dog"
-            ]
-        ]
+            "1.1 " ++ show (8>9),
+            "1.2 " ++ show (9>=0),
+            "1.3 " ++ show (True && (True || False)),
+            "1.4 " ++ show (not (not True) == True),
+            "1.5 " ++ show (countElems 'b' "It was the best of times, it was the blurst of times" == 6),
+            "1.6 " ++ show (countElems '2' "5438275439872242142" < 3),
+            "1.7 " ++ show (countElems 't' "The Importance of Being Earnest" > countElems 't' "The Picture of Dorian Gray"),
+            "1.8 " ++ show (countElems 't' "Gadsby" < countElems 's' "The quick brown fox jumped over the lazy dog"),
 
-    -- Exercise 2
-
-    let n = 2
-        xs = [True,True,True,False,False]
-
-    mapM_ putStrLn [
-            "Exercise 2 tests:",
-            "> let n = " ++ show n,
-            "> let xs = " ++ show xs,
-            unlines $ zipWith (\n x -> show n ++ ") " ++ x) [1..] [
-                "> atLeastTrueIn n xs => " ++ show (atLeastTrueIn n xs),
-                "> atMostTrueIn n xs => " ++ show (atMostTrueIn n xs),
-                "> greaterThanTrueIn n xs => " ++ show (greaterThanTrueIn n xs),
-                "> lessThanTrueIn n xs => " ++ show (lessThanTrueIn n xs),
-                "> notExactlyTrueIn n xs => " ++ show (notExactlyTrueIn n xs),
-                "> exactlyFalseIn n xs => " ++ show (exactlyFalseIn n xs)
-            ]
-        ]
-
-    -- Exercise 3
-
-    let n1 = 3
-        b1 = 2
-        xs1 = [1,2,3,4,3,2,1]
-
-    let n2 = 9
-        b2 = 'a'
-        xs2 = "A man, a plan, a canal: Panama"
-
-    mapM_ putStrLn [
-            "Exercise 3 tests:",
-            "> exactlyGivenIn " ++ show n1 ++ " " ++ show b1 ++ " " ++ show xs1 ++ " => " ++ show (exactlyGivenIn n1 b1 xs1),
-            "> exactlyGivenIn " ++ show n2 ++ " " ++ show b2 ++ " " ++ show xs2 ++ " => " ++ show (exactlyGivenIn n2 b2 xs2)
+            "2.1 " ++ show (atLeastTrueIn 2 setBool),
+            "2.2 " ++ show (atMostTrueIn 2 setBool),
+            "2.3 " ++ show (greaterThanTrueIn 2 setBool),
+            "2.4 " ++ show (lessThanTrueIn 2 setBool),
+            "2.5 " ++ show (notExactlyTrueIn 2 setBool),
+            "2.6 " ++ show (exactlyFalseIn 2 setBool),
+            
+            "3.1 " ++ show (exactlyGivenIn 3 2 setInt),
+            "3.2 " ++ show (exactlyGivenIn 9 'a' setChar)
         ]
