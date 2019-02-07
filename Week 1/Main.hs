5551d32463a46b4224ae1f6bb98f664dd318160a
Tomaszal <mrtomaszal@gmail.com>
2019-02-07 14:37:04 +0000

    Added exercise titles to "Week 1" and "Week 2".

--- Week 1/Main.hs
+++ Week 1/Main.hs
@@ -7,6 +7,7 @@ main = do
         setChar = "A man, a plan, a canal: Panama"
 
     mapM_ putStrLn [
+            "> Determine if the statements are true",
             "1.1 " ++ show (8>9),
             "1.2 " ++ show (9>=0),
             "1.3 " ++ show (True && (True || False)),
@@ -15,14 +16,16 @@ main = do
             "1.6 " ++ show (countElems '2' "5438275439872242142" < 3),
             "1.7 " ++ show (countElems 't' "The Importance of Being Earnest" > countElems 't' "The Picture of Dorian Gray"),
             "1.8 " ++ show (countElems 't' "Gadsby" < countElems 's' "The quick brown fox jumped over the lazy dog"),
-
+            "",
+            "> Comparison functions",
             "2.1 " ++ show (atLeastTrueIn 2 setBool),
             "2.2 " ++ show (atMostTrueIn 2 setBool),
             "2.3 " ++ show (greaterThanTrueIn 2 setBool),
             "2.4 " ++ show (lessThanTrueIn 2 setBool),
             "2.5 " ++ show (notExactlyTrueIn 2 setBool),
             "2.6 " ++ show (exactlyFalseIn 2 setBool),
-            
+            "",
+            "> Check for exact occurance amount",
             "3.1 " ++ show (exactlyGivenIn 3 2 setInt),
             "3.2 " ++ show (exactlyGivenIn 9 'a' setChar)
         ]
